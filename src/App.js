4c269ff3650b1f8b19a625e1c10cd81639cfe940
Cheikh Ahmadou Bamba DIAGNE <ahmadoubamba.diagne@finditech.com>
2020-05-15 19:18:13 +0000

    Pokemon with React and GraphQL

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import {PokemonContainer} from "./container/PokemonContainer";
+
+function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh/'
+  })
+  return (
+      <ApolloProvider client={client}>
+        <main>
+          <PokemonContainer/>
+        </main>
+      </ApolloProvider>
+  );
+}
+
+export default App;
